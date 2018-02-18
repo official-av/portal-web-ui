65f2e04950bab5b553e4475fed2be6203883a918
Anmol Vashistha <anmolvashisthaav@gmail.com>
2018-02-19 00:08:19 +0530

    added header styling, footer and container

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {MaterialModule} from '../material.module';
+import {FooterComponent} from './footer/footer.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-  imports: [MaterialModule],
-  exports: [HeaderComponent],
-  declarations: [HeaderComponent]
+  imports: [MaterialModule, FlexLayoutModule],
+  exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent]
 })
 export class SharedModule {
 }
