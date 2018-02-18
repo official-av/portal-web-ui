9795c357d02649f97ff012825cc592dbd6ed1f73
Anmol Vashistha <anmolvashisthaav@gmail.com>
2018-02-18 15:57:49 +0530

    added material theme, header and ui stuff

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {MaterialModule} from '../material.module';
+
+@NgModule({
+  imports: [MaterialModule],
+  exports: [HeaderComponent],
+  declarations: [HeaderComponent]
+})
+export class SharedModule {
+}
