6300e45abbbe015c498e5ee911852fc606569ec7
Anmol Vashistha <anmolvashisthaav@gmail.com>
2018-02-20 17:34:05 +0530

    added static pages, links in header, some styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AboutComponent} from './shared/static/about/about.component';
+import {HelpComponent} from './shared/static/help/help.component';
+import {ContactComponent} from './shared/static/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'help',
+    component: HelpComponent
+  }
+];
 
 @NgModule({
   imports: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,15 @@ import {HeaderComponent} from './header/header.component';
 import {MaterialModule} from '../material.module';
 import {FooterComponent} from './footer/footer.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {AboutComponent} from './static/about/about.component';
+import {ContactComponent} from './static/contact/contact.component';
+import {HelpComponent} from './static/help/help.component';
+import {AppRoutingModule} from '../app-routing.module';
 
 @NgModule({
-  imports: [MaterialModule, FlexLayoutModule],
-  exports: [HeaderComponent, FooterComponent],
-  declarations: [HeaderComponent, FooterComponent]
+  imports: [MaterialModule, FlexLayoutModule, AppRoutingModule],
+  exports: [HeaderComponent, FooterComponent, AboutComponent, ContactComponent, HelpComponent],
+  declarations: [HeaderComponent, FooterComponent, AboutComponent, ContactComponent, HelpComponent]
 })
 export class SharedModule {
 }
