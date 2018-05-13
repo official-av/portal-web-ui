4346987c1ef0700f4118d04da587103be5670d08
Anmol Vashistha <anmolvashisthaav@gmail.com>
2018-05-13 21:30:04 +0530

    added dept autocomplete

--- src/app/core/create/create.component.ts
+++ src/app/core/create/create.component.ts
@@ -3,7 +3,6 @@ import {Dept} from '../models/dept.model';
 import {CoreService} from '../core.service';
 import {Question} from '../models/question.model';
 import {NgForm} from '@angular/forms';
-import {Observable} from 'rxjs/Observable';
 import {SharedService} from '../../shared/shared.service';
 import {ToastsManager} from 'ng2-toastr';
 import {ErrorHandlerService} from '../../shared/error-handler.service';
@@ -14,19 +13,28 @@ import {ErrorHandlerService} from '../../shared/error-handler.service';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
-  depts: Observable<Dept[]>;
+  depts: Dept[];
+  dept: string;
+  filteredDepts: Dept[];
   question: Question;
 
   constructor(private coreService: CoreService, private sharedService: SharedService,
               private toastr: ToastsManager, private errorHandlerService: ErrorHandlerService) {
-    this.depts = this.sharedService.getDeptList();
     this.question = new Question();
   }
 
   ngOnInit() {
+    this.sharedService.getDeptList().subscribe((value: Dept[]) => {
+      this.depts = value;
+    });
+  }
+
+  filter(val: any) {
+    this.filteredDepts = this.depts.filter(dept => dept.name.toLowerCase().includes(val.toLowerCase()));
   }
 
   onSubmit(f: NgForm) {
+    this.question.asked_to = this.sharedService.getDeptId(this.dept);
     console.log(this.question);
     this.coreService.sendQuestion(this.question).then(() => {
       f.reset();
